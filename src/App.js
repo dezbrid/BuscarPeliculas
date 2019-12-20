65bef40f5bad7e34428ac94a9faf9c3aff6246f7
dezbrid <dezbrid@gmail.com>
2019-12-19 21:42:32 -0500

    estructura limpia

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+
+import React from 'react';
+import {
+
+  View,
+  Text,
+
+} from 'react-native';
+import Home from './views/Home'
+
+
+
+const App = () => (
+  <View>
+    <Home/>
+  </View>
+);
+
+
+
+
+export default App;
---
+++ src/views/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+
+  View,
+  Text,
+
+} from 'react-native';
+
+export default () => {
+    return(
+    <View>
+        <Text>home</Text>
+    </View>
+    );
+}
